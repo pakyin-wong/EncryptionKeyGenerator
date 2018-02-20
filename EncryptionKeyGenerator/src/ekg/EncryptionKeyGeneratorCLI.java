0e4fe74f3f30fcf2e82c36199847e6945d54506a
pakyin-wong <36594148+pakyin-wong@users.noreply.github.com>
2018-02-21 00:29:23 +0800

    interim update

--- EncryptionKeyGenerator/src/ekg/EncryptionKeyGeneratorCLI.java
+++ EncryptionKeyGenerator/src/ekg/EncryptionKeyGeneratorCLI.java
@@ -2,10 +2,13 @@ package ekg;
 
 public class EncryptionKeyGeneratorCLI {
 	public static void main(String args[]){
+		EncryptionKeyGenerator ekg = new EncryptionKeyGenerator();
+		String out = ekg.encodeToBase64String("eeeee".getBytes());
+		System.out.println(out);
+		out = new String(ekg.decodeBase64String(out));
+		System.out.println(out);
 		
 	}
 	
-	public void encrypt(){
-		
-	}
+	
 }
